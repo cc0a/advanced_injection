4a33794e4c400297d54f7cc978a4b2806418cd35
john smith <snow@snow.snow>
2025-02-17 09:44:37 -0700

    ldap directory v0.11

--- ldap_injection/ldap_injection.sh
+++ ldap_injection/ldap_injection.sh
@@ -7,11 +7,11 @@ headers=(
   "-H" "Cache-Control: max-age=0"
   "-H" "Accept-Language: en-US,en;q=0.9"
   "-H" "Upgrade-Insecure-Requests: 1"
-  "-H" "Origin: http://94.237.58.96:49697"
+  "-H" "Origin: http://ip:port/"
   "-H" "Content-Type: application/x-www-form-urlencoded"
   "-H" "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.120 Safari/537.36"
   "-H" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-  "-H" "Referer: http://94.237.58.96:49697/"
+  "-H" "Referer: http://ip:port/"
   "-H" "Accept-Encoding: identity"  # Disable automatic gzip compression
   "-H" "Cookie: PHPSESSID=gab82kje0qe5o0urm8cq3btc5d"
   "-H" "Connection: keep-alive"
